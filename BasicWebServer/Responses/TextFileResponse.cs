823714dafe8b4da9765670421cd8f86e56226158
Mihail Nalbantov <misho_1999@students.softuni.bg>
2022-02-08 09:25:29 +0000

    Remove usings

--- BasicWebServer/Responses/TextFileResponse.cs
+++ BasicWebServer/Responses/TextFileResponse.cs
@@ -1,10 +1,5 @@
 ﻿using BasicWebServer.HTTP;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BasicWebServer.Responses
 {
